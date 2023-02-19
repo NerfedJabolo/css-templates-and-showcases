fc4f03abd4862cb899270e0c198b9d4ca7cfcf14
NerfedJabolo <ezdlprofit1@gmail.com>
2023-02-19 10:05:53 +0200

    Added animated-glowing-border showcase

---
+++ animated-glowing-border/animated-glowing-border.js
@@ -0,0 +1,12 @@
+(function setGloweffectRx() {
+  const glowEffects = document.querySelectorAll('.glow-effect');
+
+  glowEffects.forEach((glowEffect) => {
+    const glowLines = glowEffect.querySelectorAll('rect');
+    const rx = getComputedStyle(glowEffect).borderRadius;
+
+    glowLines.forEach(line => {
+        line.setAttribute('rx', rx);
+    })
+  });
+})();
